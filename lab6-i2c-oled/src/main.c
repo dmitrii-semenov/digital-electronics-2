4ea75cafe1b20abf5d3628b666d40ae44848b040
Dmitrii Semenov <240689@vutbr.cz>
2023-11-01 12:40:55 +0100

    Edited

--- lab6-i2c-oled/src/main.c
+++ lab6-i2c-oled/src/main.c
@@ -1,40 +1,117 @@
-// https://github.com/Sylaina/oled-display
+/* Defines -----------------------------------------------------------*/
+#ifndef F_CPU
+# define F_CPU 16000000  // CPU frequency in Hz required for UART_BAUD_SELECT
+#endif
 
+
+/* Includes ----------------------------------------------------------*/
+#include <avr/io.h>         // AVR device-specific IO definitions
+#include <avr/interrupt.h>  // Interrupts standard C library for AVR-GCC
+#include "timer.h"          // Timer library for AVR-GCC
+#include <twi.h>            // I2C/TWI library for AVR-GCC
+#include <uart.h>           // Peter Fleury's UART library
+#include <stdlib.h>         // C library. Needed for number conversions
 #include <oled.h>
+
+struct DHT_values_structure {
+   uint8_t hum_int;
+   uint8_t hum_dec;
+   uint8_t temp_int;
+   uint8_t temp_dec;
+   uint8_t checksum;
+} dht12;
+
+// Flag for printing new data from sensor
+volatile uint8_t new_sensor_data = 0;
+
+
+// Slave and internal addresses of temperature/humidity sensor DHT12
+#define SENSOR_ADR 0x5c
+#define SENSOR_HUM_MEM 0
+#define SENSOR_TEMP_MEM 2
+#define SENSOR_CHECKSUM 4
 
 int main(void)
 {
+    char string[2];  // String for converting numbers by itoa()
+
+    // TWI
+    twi_init();
+
+    // UART
+    uart_init(UART_BAUD_SELECT(115200, F_CPU));
+
+    sei();  // Needed for UART
+
+    // Timer1
+    TIM1_OVF_1SEC
+    TIM1_OVF_ENABLE
+
     oled_init(OLED_DISP_ON);
     oled_clrscr();
 
-    oled_charMode(DOUBLESIZE);
-    oled_puts("OLED disp.");
-
     oled_charMode(NORMALSIZE);
-
-    // oled_gotoxy(x, y)
     oled_gotoxy(0, 2);
-    oled_puts("128x64, SHH1106");
-
-    // oled_drawLine(x1, y1, x2, y2, color)
-    oled_drawLine(0, 25, 120, 25, WHITE);
-
-    oled_gotoxy(0, 4);
-    oled_puts("BPC-DE2, Brno");
-
-    oled_drawLine(0, 50, 55, 50, WHITE);
-    oled_drawLine(0, 60, 30, 60, WHITE);
-    oled_drawLine(40, 45, 40, 60, WHITE);
-    oled_drawCircle(30, 50, 4, WHITE);
-    oled_fillCircle(80, 55, 5, WHITE);
-    oled_drawRect(90, 30, 100, 50, WHITE);
-
-    // Copy buffer to display RAM
+    oled_puts("Temperature");
     oled_display();
 
-    while (1) {
-        ;
+    
+
+    // Test if sensor is ready
+    if (twi_test_address(SENSOR_ADR) == 0) {
+        oled_gotoxy(0, 0);
+        oled_puts("I2C sensor detected");
+        oled_display();
+    }
+    else {
+        oled_puts("[ERROR] I2C device not detected");
+        while (1);
     }
 
+    sei();
+
+    // Infinite loop
+    while (1) {
+        if (new_sensor_data == 1) {
+            itoa(dht12.temp_int, string, 10);
+            oled_gotoxy(0, 3);
+            oled_puts(string);
+            oled_puts(".");
+            itoa(dht12.temp_dec, string, 10);
+            oled_puts(string);
+            oled_puts(" C");
+            oled_display();
+            // Do not print it again and wait for the new data
+            new_sensor_data = 0;
+        }
+    }
+
+    // Will never reach this
     return 0;
+}
+
+
+/* Interrupt service routines ----------------------------------------*/
+/**********************************************************************
+* Function: Timer/Counter1 overflow interrupt
+* Purpose:  Read temperature and humidity from DHT12, SLA = 0x5c.
+**********************************************************************/
+
+ISR(TIMER1_OVF_vect)
+{
+    // Test ACK from sensor
+    twi_start();
+    if (twi_write((SENSOR_ADR<<1) | TWI_WRITE) == 0) {
+        // Set internal memory location
+        twi_write(SENSOR_TEMP_MEM);
+        twi_stop();
+        // Read data from internal memory
+        twi_start();
+        twi_write((SENSOR_ADR<<1) | TWI_READ);
+        dht12.temp_int = twi_read(TWI_ACK);
+        dht12.temp_dec = twi_read(TWI_NACK);
+
+        new_sensor_data = 1;
+    }
+    twi_stop();
 }
